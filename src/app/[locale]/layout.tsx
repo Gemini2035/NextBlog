668c93fbf95049a58768955a29462f3ad57f7ab4
张志洋 <zhangzhiyang@sobey.com>
2025-09-22 17:53:47 +0800

    feat: intl with route finished

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,6 +1,9 @@
-// src/app/[lang]/layout.tsx
-import { NextIntlClientProvider } from 'next-intl';
+// src/app/[locale]/layout.tsx
+import { NextIntlClientProvider, hasLocale } from 'next-intl';
 import type { ReactNode } from 'react';
+import { notFound } from 'next/navigation';
+import { routing } from '@/i18n/routing';
+import { getMessages } from 'next-intl/server';
 
 interface LocaleLayoutProps {
     children: ReactNode;
@@ -9,12 +12,16 @@ interface LocaleLayoutProps {
 
 export default async function RootIntlLayout({ children, params }: LocaleLayoutProps) {
     const { locale } = await params;
-    console.log(locale);
+
+    if (!hasLocale(routing.locales, locale)) {
+        notFound();
+    }
+
+    const messages = await getMessages();
 
     return (
-        <NextIntlClientProvider >
+        <NextIntlClientProvider messages={messages}>
             {children}
         </NextIntlClientProvider>
-
     );
 }
