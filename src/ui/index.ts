ede47dc2d70f2a11ac7b200ad3afe42e81a74356
张志洋 <zhangzhiyang@sobey.com>
2025-09-24 14:39:52 +0800

    perf: optimized exportation in ui button

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,7 +1,3 @@
-// UI组件库入口文件 - 简化版
-// 导出所有UI组件
-
-// 导出Button组件
 export { Button } from './Button'
 export type { 
   ButtonProps, 
@@ -9,11 +5,3 @@ export type {
   ButtonSize,
   ButtonRef
 } from './Button'
-
-// 导出Button样式工具
-export {
-  getVariantStyles,
-  getSizeStyles,
-  getRoundedStyles,
-  getButtonStyles,
-} from './Button/styles'
